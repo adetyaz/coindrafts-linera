22b155e9b204d22a5b8cd0726fad9a923c9c206c
adetyaz <tuneymitch@gmail.com>
2026-02-03 00:59:23 +0100

    chore: clean unused code

--- backend/applications/simple-todo/src/state.rs
+++
@@ -1,9 +0,0 @@
-use linera_sdk::views::{linera_views, MapView, RegisterView, RootView, ViewStorageContext};
-use simple_todo::TodoItem;
-
-#[derive(RootView)]
-#[view(context = ViewStorageContext)]
-pub struct SimpleTodoState {
-    pub todos: MapView<u64, TodoItem>,
-    pub next_id: RegisterView<u64>,
-}
